642e2359fdefd3dd658ed446ffa6f281c4e2c6a8
anil verma <anilcs240@gmail.com>
2021-10-24 17:24:59 +0530

    basic slack

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/database";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBAc7Kxges6cmGZgPQFvjL9pMgaETVYKOU",
-    authDomain: "react-chat-app-a5633.firebaseapp.com",
-    projectId: "react-chat-app-a5633",
-    storageBucket: "react-chat-app-a5633.appspot.com",
-    messagingSenderId: "638442158432",
-    appId: "1:638442158432:web:981cb245d2307b5ac10605",
-    measurementId: "G-2021M7E1XT",
-    databaseURL: "react-chat-app-a5633-default-rtdb.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    databaseURL: ""
 };
 
 // Initialize Firebase
